d437c7bf3b506c82edc18b73d6b2d30a7f038898
klaus <k-Laus@gmx.net>
2022-11-21 21:39:43 +0100

    Return json with example model

--- src/main/java/com/example/plantumlspringerver/PlantUmlController.java
+++ src/main/java/com/example/plantumlspringerver/PlantUmlController.java
@@ -1,8 +1,5 @@
 package com.example.plantumlspringerver;
 
-import java.time.LocalDateTime;
-import java.util.Base64;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +8,7 @@ public class PlantUmlController {
 	
 	@PostMapping("/encode")
 	public String encode() {
-		return Base64.getEncoder().encodeToString(LocalDateTime.now().toString().getBytes());
+		return "{ \"modelEncoded\": \"SyfFKj2rKt3CoKnELR1Io4ZDoSa70000!\" }";
 	}
 
 }
